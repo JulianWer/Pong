92935f48173d2be49e5adb9da8e01eebec8d7c3d
Superjuli <68253986+Superjuli@users.noreply.github.com>
2021-11-02 15:47:56 +0100

    Fertiges Pong

---
+++ pong/src/pong/Main.java
@@ -0,0 +1,17 @@
+package pong;
+import javax.swing.JFrame;
+
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        
+        
+        JFrame frame = new JFrame();
+        demopanel panel = new demopanel();
+        frame.addKeyListener(new keypress(panel));
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,400);
+        frame.setVisible(true);
+    }
+}
---
+++ pong/src/pong/keypressed.java
@@ -0,0 +1,38 @@
+package pong;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
+
+class keypress extends KeyAdapter {
+
+    
+    demopanel panel;
+    public keypress(demopanel panel){
+        this.panel = panel;
+    }
+
+    
+    public void keyPressed(KeyEvent evt) {
+
+        if (evt.getKeyChar() == 's') {
+            panel.moveDownP1();
+        }
+        if (evt.getKeyChar() == 'w') {
+            panel.moveUpP1();
+        }
+        if (evt.getKeyCode() == KeyEvent.VK_DOWN) {
+            panel.moveDownP2();
+        }
+        if (evt.getKeyCode() == KeyEvent.VK_UP) {
+            panel.moveUpP2();
+        }
+        if (evt.getKeyCode() == KeyEvent.VK_SPACE) {
+            panel.timer.start();
+        }
+        
+        
+        
+    }
+
+    
+    
+}
