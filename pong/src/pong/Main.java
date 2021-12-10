0739b834fe81494b6501e256a7d21fb3af61df7c
Superjuli <68253986+Superjuli@users.noreply.github.com>
2021-12-10 15:50:33 +0100

    update

--- pong/src/pong/Main.java
+++ pong/src/pong/Main.java
@@ -1,25 +1,16 @@
 package pong;
 
+import java.awt.Window;
+
 import javax.swing.JFrame;
 
 
 public class Main{
     public static void main(String[] args) throws Exception{
     	
-    	//Which score to win?
-    	UI ui = new UI();
-    	int input = ui.getInput();
-        
-    	
-    	//Game
-        JFrame frame = new JFrame();
-        Panel panel = new Panel(input);
-        frame.addKeyListener(new Key_pressed(panel));
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,400);
-        frame.setVisible(true);
-        
+		UI ui = new UI();
         
     }
+    
+    
 }
--- pong/src/pong/UI.java
+++ pong/src/pong/UI.java
@@ -1,28 +1,51 @@
 package pong;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class UI {
 	
+	
+
 	private Scanner sc = new Scanner(System.in);
 	private int input;
+	public boolean gameTime = false;
+	
 	
 	public UI() {
 		this.ask();
 	}
 	
 	public void ask() {
-	System.out.println("Welcome to Pong!");
-	System.out.println("Player one controls the bat with 'w' and 's '  \nPlayer two controls the bat with 'UP' and 'DOWN'");
-	System.out.println("Wenn Sie 10 Ballwechsel erreichen dann wird der Ball rot und zählt 2 Punkte");
-	System.out.print("Win number: ");
-	input = sc.nextInt();
-	sc.close();
+		JFrame popUp = new JFrame("Beginning");
+    	popUp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	JTextField txt = new JTextField();
+    	//set size of the text box
+    	txt.setBounds(50, 100, 100, 30);
+    	JButton button = new JButton(" >> CLICK ME <<");
+		button.setBounds(40, 40, 200, 40);
+    	popUp.add(txt);
+    	popUp.add(button);
+    	button.addActionListener(new ActionListener() {
+
+    	    public void actionPerformed(ActionEvent e) {
+    	        //your actions
+    	    	input = Integer.parseInt(txt.getText());
+    	    	System.out.println(input);
+    	    	//Game wird nach dem pressen und eingeben aufgerufen
+    	    	Game win = new Game(input);
+    	    	
+    	    }
+    	});
+    	popUp.setSize(400,200);
+    	popUp.setLayout(null);
+    	popUp.setVisible(true);
 	}
-	
-	public int getInput() {
-		return input;
-		
-	}
+
 	
 }
