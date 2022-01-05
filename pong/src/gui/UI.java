a5e740b12b3da64b221d6bd2815085401cf33a41
julianwernz <julianwernz@mbp-von-julian.fritz.box>
2022-01-05 17:44:59 +0100

    Optimierung Design UI, erstellen Methode für Label.

--- pong/src/gui/UI.java
+++ pong/src/gui/UI.java
@@ -24,12 +24,14 @@ public class UI extends JFrame implements Serializable {
 	
 		
 		
-		JLabel label = new JLabel("Bis zu welchem Punktestand soll das Spiel gehen?");
-		label.setFont(new Font("SansSerif", Font.PLAIN, 15));
-		label.setForeground(Color.WHITE);
-		label.setBounds(30,300, 400,40);
+		
     	JTextField txt = new JTextField();
     	txt.setBounds(50, 350, 200, 40);
+    	JTextField plyr1 = new JTextField();
+		plyr1.setBounds(50, 200, 100, 40);
+		JTextField plyr2 = new JTextField();
+		plyr2.setBounds(200, 200, 100, 40);
+		
     	
     	JButton button = new JButton("play");
 		button.setBackground(new Color(59, 89, 182));
@@ -39,24 +41,17 @@ public class UI extends JFrame implements Serializable {
 		button.setBounds(250, 350, 70, 40);
     	
 		
-    	JTextField plyr1 = new JTextField();
-		plyr1.setBounds(50, 200, 100, 40);
-		JTextField plyr2 = new JTextField();
-		plyr2.setBounds(200, 200, 100, 40);
 		
-		JLabel lastplay = new JLabel("Leztes Spiel: " + ps.loadData().get(1).getName() + " " + ps.loadData().get(0).getName() ) ;
-		lastplay.setForeground(Color.WHITE);
-		lastplay.setFont(new Font("SansSerif", Font.PLAIN, 15));
-		lastplay.setBounds(30,100, 400,40);
-		
-		
-		add(lastplay);
+		add(createLabel("PONG", 150, 20, 400, 40,30));
+		add(createLabel("Leztes Spiel: " + ps.loadData().get(1).getName() + ", " + ps.loadData().get(0).getName(), 30, 100, 400, 40,15));
+		add(createLabel("Player 1" ,70, 170, 100, 40,15));
+		add(createLabel("Player 2" ,220, 170, 100, 40,15));
     	add(txt);
 		add(plyr1);
 		add(plyr2);
     	add(button);
 		getRootPane().setDefaultButton(button);
-		add(label);
+		add(createLabel("Bis zu welchem Punktestand soll das Spiel gehen?", 30,300, 400,40,15));
     	bAction(button,txt,plyr1,plyr2);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	setSize(400,450);
@@ -66,6 +61,14 @@ public class UI extends JFrame implements Serializable {
     	setVisible(true);
     	
 
+	}
+	
+	public JLabel createLabel(String input,int x ,int y, int w, int h, int tall) {
+		JLabel label = new JLabel(input) ;
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Arial", Font.PLAIN, tall));
+		label.setBounds(x,y, w,h);
+		return label;
 	}
 
 	public void bAction(JButton button, JTextField txt ,JTextField plyr1,JTextField plyr2 ) {
